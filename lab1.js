8888b4e88cf16fcc3dd8b992191ceb81ba571c18
salobando <salobando2023@gmail.com>
2025-10-29 14:56:38 -0500

    Segundo punto ingresar datos

--- lab1.js
+++ lab1.js
@@ -6,4 +6,17 @@ let sinValor;
 let valorNuevo = null;
 
 console.log(sinValor +" - "+ valorNuevo);
- 
+
+//! 2) Ingreso de datos por teclado
+const prompt = require("prompt-sync")();
+
+let nacimiento = prompt("Escriba su año de nacimiento: ");
+let yearActual = new Date().getFullYear();
+let edadUs = yearActual - nacimiento; 
+console.log("Su edad es "+ edadUs);
+
+let num1 = Number(prompt("Ingrese un número: "));
+let num2 = Number(prompt("Ingrese otro número: "));
+let suma = num1+num2;
+console.log("La suma es "+ suma);
+
