ddd16ca69efca7244e89e4bea330c4caf2712ec5
salobando <salobando2023@gmail.com>
2025-10-30 14:49:56 -0500

    Ejercicio2:Validar contraseña

--- lab2.js
+++ lab2.js
@@ -8,3 +8,15 @@ function calcularPropina(total, porcentaje){
 let total = prompt("Ingrese el total de la cuenta: ");
 let porcentaje = prompt("Ingrese el porcentaje de la propina: ");
 console.log(calcularPropina(total,porcentaje));
+
+//! Ejercicio 2: Validar contraseña
+function validarContrasena(contra){
+    if(contra.length >=8 & /\d/.test(contra) & contra !== contra.toLowerCase()){
+       return true;     
+    }
+    return false;
+}
+
+prompt("Ingrese un usuario: ");
+let contra = prompt("Ingrese una contraseña: ");
+console.log(validarContrasena(contra));
