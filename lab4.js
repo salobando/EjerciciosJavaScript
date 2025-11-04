d9c4a47d9738f90f8620926b604febd4e9f07f6f
salobando <salobando2023@gmail.com>
2025-11-04 14:48:55 -0500

    Ejercicio 3.Sistema productos y 4.Sistema de Figuras

--- lab4.js
+++ lab4.js
@@ -6,8 +6,8 @@ class Vehiculo{
         this.año = año;
     }
     obtenerDetalles(){
-    console.log(`El vehiculo tiene las siguientes caracteristicas:
-        marca: ${this.marca} Modelo: ${this.modelo} Año:${this.año}`);
+        return `El vehiculo tiene las siguientes caracteristicas:
+        marca: ${this.marca} Modelo: ${this.modelo} Año:${this.año}`;
     }
 }
 
@@ -71,3 +71,77 @@ const desarrollador1 = new Desarrollador("Alexa", 28,3400000, "Java");
 console.log("Salario del gerente: "+ gerente1.calcularSalario());
 console.log("Salario del desarrollador: "+ desarrollador1.calcularSalario());
 
+//! 3. Sistema de productos electronicos
+class Producto{
+    constructor(nombre,precio,marca){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.marca = marca;
+    }
+    descripcion(){
+        return `Detalles del producto:
+        Nombre: ${this.nombre} Precio: ${this.precio} Marca:${this.marca}`;        
+    }
+}
+
+class Telefono extends Producto{
+    constructor(nombre,precio,marca,almacenamiento,ram){
+        super(nombre,precio,marca);
+        this.almacenamiento = almacenamiento;
+        this.ram = ram;
+    }    
+    descripcion(){
+        return `${super.descripcion()} Almacenamiento: ${this.almacenamiento} Ram:${this.ram}`;
+    }
+}
+class Laptop extends Producto{
+    constructor(nombre,precio,marca,procesador,pulgadas){
+        super(nombre,precio,marca);
+        this.procesador = procesador;
+        this.pulgadas = pulgadas;
+    }
+    descripcion(){
+        return `${super.descripcion()} Procesador: ${this.procesador} Pulgadas:${this.pulgadas}`;        
+    }    
+}
+
+const tel = new Telefono("Galaxy A15",620000,"Samsung", "256GB",8);
+const lapto = new Laptop("Portatil HP",1999000,"HP", "Intel Core i5",15);
+console.log(tel.descripcion());
+console.log(lapto.descripcion());
+
+//! 4. Sistema de figuras geométricas
+class Figura{
+    constructor(color){
+        this.color = color;
+    }
+    area(){
+        return 0;
+    }
+}
+
+class Rectangulo extends Figura{
+    constructor(color,base,altura){
+        super(color);
+        this.base = base;
+        this.altura = altura;
+    }
+    area(){
+        return this.altura*this.base;
+    }
+}
+
+class Circulo extends Figura{
+    constructor(color,radio){
+        super(color);
+        this.radio = radio;
+    }
+    area(){
+        return Math.PI*Math.pow(this.radio,2);
+    }
+}
+
+const rect = new Rectangulo("Rojo",5,8);
+const circ = new Circulo("Azul",5);
+console.log("Area del Rectangulo: "+rect.area());
+console.log("Area del Circulo: "+circ.area());
